d0814aea01e7c9f37525ebdf166b928fb0a2783f
gumasushi <stanliestarkman@gmail.com>
2026-06-15 23:25:14 +0800

    Unit testing

--- backend/src/NotSpotify.Api/obj/Debug/net8.0/NotSpotify.Api.AssemblyInfo.cs
+++ backend/src/NotSpotify.Api/obj/Debug/net8.0/NotSpotify.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotSpotify.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cd3164ea93885cb7a89815f68f3e905f0c1998c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26122bba570753da5f47db59e6e452a98cd7f2ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotSpotify.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotSpotify.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

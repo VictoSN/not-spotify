41d3c7b6eda98c1fd28a6b3921a90cd162670a91
gumasushi <stanliestarkman@gmail.com>
2026-06-19 22:45:17 +0800

    1A waveform

--- backend/src/NotSpotify.Api/obj/Debug/net8.0/NotSpotify.Api.AssemblyInfo.cs
+++ backend/src/NotSpotify.Api/obj/Debug/net8.0/NotSpotify.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotSpotify.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+059903b2e372cfa2747f493cb41376a141765495")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0b5bb5d8c652b51f6bb4fc96db2ccd4d91b68c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotSpotify.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotSpotify.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
